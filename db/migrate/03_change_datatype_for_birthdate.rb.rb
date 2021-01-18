8d616d180d8d4a88801254c2dd144f8cebc03770
Leo <efrenn98@gmail.com>
2021-01-17 18:36:16 -0800

    Done.

--- db/migrate/03_change_datatype_for_birthdate.rb.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb.rb
@@ -1,7 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
     def change
     change_table :create_students do |t|
-        change_column(create_students,birthday, integer) 
+        t.change :birthday, :integer
         end 
     end
 end
